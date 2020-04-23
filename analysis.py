9f1392cafb428311f23089b600763f128ef5f4f8
agentcap <sreeramsaipradeep@gmail.com>
2020-04-23 23:27:59 +0530

    Updated Analysis.py

--- analysis.py
+++ analysis.py
@@ -14,32 +14,39 @@ def generate_graph(proc, tseq, tsrd, t1d, t2d, title=None):
 	plt.show()
 
 ## x: processes, y: average execution time, variation of different BFS algorithms for same given graph.
-rp = [1, 5, 5, 5, 6, 10, 10, 10]
-cp = [1, 1, 2, 4, 5, 5, 8, 10]
-proc = [1, 5, 10, 20, 30, 50, 80, 100]
+rp = [1, 1, 2, 2, 2, 10, 10, 10]
+cp = [1, 2, 2, 3, 4, 10, 5, 8, 10]
+proc = [1, 2, 4, 6, 8, 80, 100]
 
+rp = rp[:5]
+cp = cp[:5]
+proc = proc[:5]
 
-
-v1 = 10
+v1 = 20
 k1 = 2
-t1d = [1,2,3,4,5,6,1,2]
-t2d = [6,5,3,3,2,1,1,4]
-tseq = [2,3,4,5,1,2,1,5]
-tsrd = [3,1,2,3,4,5,1,1]
-# for i in range(len(proc)):
-# 	print(i, proc[i])
-# 	print('python test.py ' +  str(v1) + ' ' + str(v1*k1) + ' ' + str(rp[i]) + ' ' + str(cp[i]) +  ' analysis')
-# 	os.system('python test.py ' +  str(v1) + ' ' + str(v1*k1) + ' ' + str(rp[i]) + ' ' + str(cp[i]) +  ' analysis')
-# 	os.system('cd sequential && erlc *.erl && erl -noshell -s bfs main ../analysis/inpSeqMeta ../analysis/inpSeqAdj -s init stop > ../analysis/time.txt && cd ..')
-# 	os.system('cd shared && erlc *.erl && erl -noshell -s bfs_shared main ../analysis/inpSrdMeta ../analysis/inpSrdAdj -s init stop >> ../analysis/time.txt && cd ..')
-# 	os.system('cd 1D && erlc *.erl && erl -noshell -s bfs_1d main ../analysis/inp1D ../analysis -s init stop >> ../analysis/time.txt && cd ..')
-# 	os.system('cd 2D && erlc *.erl && erl -noshell -s bfs_2d main ../analysis/inp2D ../analysis -s init stop >> ../analysis/time.txt && cd ..')
-# 	with open('analysis/time.txt', 'r') as f:
-# 		times = f.readlines()
-# 		tseq.append(times[0].strip('\n'))
-# 		tsrd.append(times[1].strip('\n'))
-# 		t1d.append(times[2].strip('\n'))
-# 		t2d.append(times[3].strip('\n'))
+# t1d = [1,2,3,4,5,6,1,2]
+# t2d = [6,5,3,3,2,1,1,4]
+# tseq = [2,3,4,5,1,2,1,5]
+# tsrd = [3,1,2,3,4,5,1,1]
+t1d = []
+t2d = []
+tseq = []
+tsrd = []
+for i in range(len(proc)):
+# for i in range(5):
+	print(i, proc[i])
+	print('python test.py ' +  str(v1) + ' ' + str(v1*k1) + ' ' + str(rp[i]) + ' ' + str(cp[i]) +  ' analysis')
+	os.system('python test.py ' +  str(v1) + ' ' + str(v1*k1) + ' ' + str(rp[i]) + ' ' + str(cp[i]) +  ' analysis')
+	os.system('cd sequential && erlc *.erl && erl -noshell -s bfs main ../analysis/inpSeqMeta ../analysis/inpSeqAdj time -s init stop > ../analysis/time.txt && cd ..')
+	os.system('cd shared && erlc *.erl && erl -noshell -s bfs_shared main ../analysis/inpSrdMeta ../analysis/inpSrdAdj time -s init stop >> ../analysis/time.txt && cd ..')
+	os.system('cd 1D && erlc *.erl && erl -noshell -s bfs_1d main ../analysis/inp1D ../analysis time -s init stop >> ../analysis/time.txt && cd ..')
+	os.system('cd 2D && erlc *.erl && erl -noshell -s bfs_2d main ../analysis/inp2D ../analysis time -s init stop >> ../analysis/time.txt && cd ..')
+	with open('analysis/time.txt', 'r') as f:
+		times = f.readlines()
+		tseq.append(float(times[0].strip('\n').split(" ")[1]))
+		tsrd.append(float(times[1].strip('\n').split(" ")[1]))
+		t1d.append(float(times[2].strip('\n').split(" ")[1]))
+		t2d.append(float(times[3].strip('\n').split(" ")[1]))
 
 print(tseq, tsrd, t1d, t2d)
 generate_graph(proc, tseq, tsrd, t1d, t2d, 'Vertices=' + str(v1) + ', Degree=' + str(k1))
